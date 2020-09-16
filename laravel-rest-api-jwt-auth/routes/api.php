0cca5ebdcfd05c648f2ef5035bc4cd5051bebff6
bobrov76 <50507472+bobrov76@users.noreply.github.com>
2020-09-16 13:31:43 +0700

    JWT авторизация

--- laravel-rest-api-jwt-auth/routes/api.php
+++ laravel-rest-api-jwt-auth/routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post("login", [AuthController::class,'login']);
+Route::post("register", [AuthController::class,'register']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["middleware" => "auth.jwt"], function () {
+    Route::get("logout", [AuthController::class,'logout']);
+    Route::resource("tasks", TaskController::class);
 });
